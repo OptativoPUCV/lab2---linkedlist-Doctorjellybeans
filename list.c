331cf9e1f89e79cc0b67181ad22f6940c30c7c36
Replit user <>
2023-08-23 20:26:19 +0000

    new try

--- list.c
+++ list.c
@@ -67,8 +67,7 @@ void * lastList(List * list) {
 }
 
 void * prevList(List * list) {
-  
-
+  printf("current:%d prev:%d",*((int*)list->current->data),*((int*)list->current->prev->data));
   list->current = list->current->prev;
   return list->current->data;
 }
