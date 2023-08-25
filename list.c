2b5020e5daeabd5da6cad04b81ecec19fdb96843
Replit user <>
2023-08-25 23:16:33 +0000

    new try

--- list.c
+++ list.c
@@ -69,18 +69,20 @@ void * lastList(List * list) {
   return list->current->data; // Se devuelve el dato del nodo actual
 }
 
+// Función para obtener el elemento previo al nodo actual
 void * prevList(List * list) { //
   if (list->current == NULL || list->current == list->head){
-    return NULL;
+    return NULL; // No hay un elemento previo
   }
-  //printf("current:%d prev:%d",*((int*)list->current->data),*((int*)list->current->prev->data));
-  list->current = list->current->prev;
-  return list->current->data;
+  //printf("current:%d prev:%d",*((int*)list->current->data),*((int*)list->current->prev->data)); esto lo use para comprobar la lectura de data 
+  list->current = list->current->prev; // Se mueve al nodo previo
+  return list->current->data; // Se devuelve el dato del nodo actual
 }
 
+// Función para agregar un elemento al frente de la lista
 void pushFront(List * list, void * data) {
-  Node *nuevoNodo = createNode(data);
-  if (list->head == NULL){
+  Node *nuevoNodo = createNode(data); // Se crea un nuevo nodo con el dato
+  if (list->head == NULL){ // Si la lista está vacía, el nuevo nodo se convierte en la cabeza y la cola
     list->head = nuevoNodo;
     list->tail = nuevoNodo;
   } else {
@@ -91,34 +93,31 @@ void pushFront(List * list, void * data) {
 }
 
 void pushBack(List * list, void * data) {
-    list->current = list->tail;
-    pushCurrent(list,data);
+    list->current = list->tail; 
+    pushCurrent(list,data); 
 }
 
 void pushCurrent(List * list, void * data) {
   if (list == NULL){
-    return;
+    return; // La lista es nula, no se puede agregar el elemento
   }
   
-  Node *nuevoNodo = createNode(data);
+  Node *nuevoNodo = createNode(data); // Se crea un nuevo nodo con el dato
 
-  if (list->current == NULL){
-    list->head = nuevoNodo;
+  if (list->current == NULL){ // Si la lista está vacía, el nuevo nodo se convierte en la cabeza y la cola
+    list->head = nuevoNodo; 
     list->tail = nuevoNodo;
     list->current = nuevoNodo;
   } else {
-    nuevoNodo->next = list->current->next;
+    nuevoNodo->next = list->current->next; // El nuevo nodo apunta al siguiente nodo del nodo actual
     if (list->current->next != NULL)
     {
-      list->current->next->prev = nuevoNodo;
+      list->current->next->prev = nuevoNodo; // El nodo siguiente al nodo actual apunta al nuevo nodo como previo
     }
-    nuevoNodo->prev = list->current;
-    list->current->next = nuevoNodo;
-
-    
-    list->tail = nuevoNodo;
-
-    list->current = nuevoNodo;
+    nuevoNodo->prev = list->current; // El nuevo nodo apunta al nodo actual como previo
+    list->current->next = nuevoNodo; // El nodo actual apunta al nuevo nodo como siguiente
+    list->tail = nuevoNodo; // El nuevo nodo se convierte en la nueva cola
+    list->current = nuevoNodo; // El nuevo nodo se convierte en el nodo actual
   }
 
 }
@@ -133,29 +132,30 @@ void * popBack(List * list) {
     return popCurrent(list);
 }
 
+// Función para eliminar el elemento actual de la lista
 void * popCurrent(List * list) {
   if (list == NULL || list->current == NULL){
-    return NULL; 
+    return NULL; // La lista o el nodo actual son nulos, no se puede eliminar el elemento
   }
    
-  Node *eliminado = list->current;
-  void *data_local = eliminado->data;
+  Node *eliminado = list->current; // Se guarda una referencia al nodo actual
+  void *data_local = eliminado->data; // Se guarda el dato del nodo actual
   
   if (eliminado->prev != NULL){
-    eliminado->prev->next = eliminado->next;
+    eliminado->prev->next = eliminado->next; // El nodo previo apunta al siguiente nodo
   } else {
-    list->head = eliminado->next;
+    list->head = eliminado->next; // Si no hay nodo previo, el siguiente nodo se convierte en la cabeza
   }
   
   if (eliminado->next != NULL){
-    eliminado->next->prev = eliminado->prev;
+    eliminado->next->prev = eliminado->prev; // El nodo siguiente apunta al nodo previo
   } else {
-    list->tail = eliminado->prev;
+    list->tail = eliminado->prev; // Si no hay nodo siguiente, el nodo previo se convierte en la cola
   }
   
-  list->current = eliminado->next;
-  free(eliminado);
-  return data_local;
+  list->current = eliminado->next; // El nodo siguiente al eliminado se convierte en el nodo actual
+  free(eliminado); // se libera el nodo
+  return data_local; // se devuelve el dato del nodo anteriormente eliminado
 }
 
 void cleanList(List * list) {
